b2cafddc7ed81864fe7edd381056b2c1c1d9f362
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-05-27 17:18:28 -0500

    ML.NET model builder

---
+++ ml.net/MLConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using MLConsoleApp;
+
+Console.WriteLine("Ctrl+C to exit...");
+
+while (true)
+{
+    Console.WriteLine("Enter some text:");
+
+    var sampleData = new MLSentiment.ModelInput()
+    {
+        Text = Console.ReadLine()!,
+    };
+
+    var result = MLSentiment.Predict(sampleData);
+    var text = result.Prediction == 1 ? "negative" : "ok";
+    Console.WriteLine($"Result: {text}, Confidence: {result.Score[(int)result.Prediction]}");
+    Console.WriteLine();
+}
