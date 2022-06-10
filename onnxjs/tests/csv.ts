8ff75592ef61d941c32d68ea30e6dba784b484e8
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-06-10 10:03:14 -0500

    Add onnx-runtime-node and 1 test to load an .onnx file (#2)
    Context: https://onnxruntime.ai/docs/get-started/with-javascript.html
    Context: https://github.com/microsoft/onnxruntime-inference-examples
    Context: https://github.com/microsoft/onnxruntime
    
    * Added Github action for JavaScript tests
    * Updated workflow names

--- onnxjs/tests/csv.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-
-describe('csv tests', () => {
-    it('can load a csv file', async () => {
-        var fitted = 'asdf';
-        expect(fitted).to.be.not.null;
-    });
-});
