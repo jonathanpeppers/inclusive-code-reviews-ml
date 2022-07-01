eb3b7c90a74bd5174cf3689741ef94d8e7f3c40d
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-07-01 13:07:34 -0500

    Clean up console app, latest nightly ML.NET packages

--- ml.net/InclusiveCodeReviews.ConsoleApp/Program.cs
+++ ml.net/InclusiveCodeReviews.ConsoleApp/Program.cs
@@ -1,32 +1,18 @@
-//*****************************************************************************************
-//*                                                                                       *
-//* This is an auto-generated file by Microsoft ML.NET CLI (Command-Line Interface) tool. *
-//*                                                                                       *
-//*****************************************************************************************
-
 using System;
 using InclusiveCodeReviews.Model;
 
-namespace InclusiveCodeReviews.ConsoleApp
+Console.WriteLine("Ctrl+C to exit...");
+
+while (true)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Create single instance of sample data from first line of dataset for model input
-            ModelInput sampleData = new ModelInput()
-            {
-                Text = @"ship it!",
-            };
+	Console.WriteLine("Enter some text:");
 
-            // Make a single prediction on the sample data and print results
-            var predictionResult = ConsumeModel.Predict(sampleData);
+	var sampleData = new ModelInput()
+	{
+		Text = Console.ReadLine()!,
+	};
 
-            Console.WriteLine("Using model to make single prediction -- Comparing actual Isnegative with predicted Isnegative from sample data...\n\n");
-            Console.WriteLine($"Text: {sampleData.Text}");
-            Console.WriteLine($"\n\nPredicted Isnegative value {predictionResult.Prediction} \nPredicted Isnegative scores: [{String.Join(",", predictionResult.Score)}]\n\n");
-            Console.WriteLine("=============== End of process, hit any key to finish ===============");
-            Console.ReadKey();
-        }
-    }
+	var result = ConsumeModel.Predict(sampleData);
+	Console.WriteLine($"IsNegative: {result.Prediction}, Confidence: {result.Score[result.Prediction == "1" ? 1 : 0]}");
+	Console.WriteLine();
 }
