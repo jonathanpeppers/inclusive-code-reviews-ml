a6fcbe717a28a340118212fecc2db387ea9d19a2
Rui Marinho <me@ruimarinho.net>
2022-09-21 19:57:46 +0100

    [MLTrainer] Add keyboard support to classify comments

--- Maui/MLTrainer/AppShell.xaml.cs
+++ Maui/MLTrainer/AppShell.xaml.cs
@@ -1,9 +1,55 @@
-﻿namespace MLTrainer;
+﻿using Microsoft.UI.Xaml;
+
+namespace MLTrainer;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
+	ViewModels.ClassificationViewModel _classificationViewModel;
+	public AppShell(ViewModels.ClassificationViewModel classificationViewModel)
 	{
 		InitializeComponent();
+		BindingContext = _classificationViewModel = classificationViewModel;
+	}
+
+	protected override void OnNavigated(ShellNavigatedEventArgs args)
+	{
+		base.OnNavigated(args);
+
+		Window.Activated += AppShellActivated;
+		Window.Deactivated += AppShellDeactivated;
+	}
+
+	void AppShellDeactivated(object? sender, EventArgs e)
+	{
+#if WINDOWS
+		MauiWinUIWindow? win = Window.Handler.PlatformView as MauiWinUIWindow;
+		if (win?.Content != null)
+			win.Content.KeyDown -= ContentKeyDown;
+#endif
+	}
+
+	void AppShellActivated(object? sender, EventArgs e)
+	{
+#if WINDOWS
+		MauiWinUIWindow? win = Window.Handler.PlatformView as MauiWinUIWindow;
+		if (win?.Content != null)
+			win.Content.KeyDown += ContentKeyDown;
+#endif
+	}
+
+	void ContentKeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+	{
+		if (e.Key == Windows.System.VirtualKey.Left)
+		{
+			_classificationViewModel.BadCommentCommand.Execute(null);
+		}
+		if (e.Key == Windows.System.VirtualKey.Right)
+		{
+			_classificationViewModel.GoodCommentCommand.Execute(null);
+		}
+		if (e.Key == Windows.System.VirtualKey.Space)
+		{
+			_classificationViewModel.SkipCommentCommand.Execute(null);
+		}
 	}
 }
