326ba0b253dd08c283db9c411089f4c33c2c7a80
Rui Marinho <me@ruimarinho.net>
2022-09-21 23:07:52 +0100

    [MLTrainer] Add keyboard support to classify comments (#74)
    Allow to use keyboard to classify comments faster.
    
    Left key will do a bad comment, right key will classify as a good comment, and space will skip the comment.
    
    Use `#if WINDOWS` for this feature.

--- Maui/MLTrainer/AppShell.xaml.cs
+++ Maui/MLTrainer/AppShell.xaml.cs
@@ -2,8 +2,52 @@
 
 public partial class AppShell : Shell
 {
-	public AppShell()
+	ViewModels.ClassificationViewModel _classificationViewModel;
+	public AppShell(ViewModels.ClassificationViewModel classificationViewModel)
 	{
 		InitializeComponent();
+		BindingContext = _classificationViewModel = classificationViewModel;
 	}
+
+	protected override void OnNavigated(ShellNavigatedEventArgs args)
+	{
+		base.OnNavigated(args);
+#if WINDOWS
+		Window.Activated += AppShellActivated;
+		Window.Deactivated += AppShellDeactivated;
+	}
+
+	void AppShellDeactivated(object? sender, EventArgs e)
+	{
+
+		MauiWinUIWindow? win = Window.Handler.PlatformView as MauiWinUIWindow;
+		if (win?.Content != null)
+			win.Content.KeyDown -= ContentKeyDown;
+	}
+
+	void AppShellActivated(object? sender, EventArgs e)
+	{
+		MauiWinUIWindow? win = Window.Handler.PlatformView as MauiWinUIWindow;
+		if (win?.Content != null)
+			win.Content.KeyDown += ContentKeyDown;
+	}
+
+	void ContentKeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+	{
+		if (e.Key == Windows.System.VirtualKey.Left)
+		{
+			_classificationViewModel.BadCommentCommand.Execute(null);
+		}
+		if (e.Key == Windows.System.VirtualKey.Right)
+		{
+			_classificationViewModel.GoodCommentCommand.Execute(null);
+		}
+		if (e.Key == Windows.System.VirtualKey.Space)
+		{
+			_classificationViewModel.SkipCommentCommand.Execute(null);
+		}
+	}
+#else
+	}
+#endif
 }
